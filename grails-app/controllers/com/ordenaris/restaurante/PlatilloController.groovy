c3a2f51312b6ee68790a83eac30299cc3c059bfd
Carlicsus <carlicsus3@gmail.com>
2025-10-06 22:52:25 -0600

    Controller de platillos
    Update: platilloController - Se crean los controladores para editar platillo && editar/desactivar/eliminar status platillo && listar informacion de un platillo, platilloService - se crean los metodos correspondientes a ser usados en el controlador de platillo

--- grails-app/controllers/com/ordenaris/restaurante/PlatilloController.groovy
+++ grails-app/controllers/com/ordenaris/restaurante/PlatilloController.groovy
@@ -62,22 +62,7 @@ class PlatilloController {
             }
             platillosDisponibles = data.platillosDisponibles
         }
-        // println "-----------"
-        // println data.nombre
-        // println data.menuTipo 
-        // println fechaDisponible
-        // println data.costo.toInteger()
-        // println data.descripcion
-        // println platillosDisponibles
-        // println "-----------"
-        // println data.nombre.getClass()
-        // println data.menuTipo.getClass()
-        // println fechaDisponible.getClass()
-        // println data.costo.toInteger().getClass()
-        // println data.descripcion.getClass()
-        // println platillosDisponibles.toInteger().getClass()
         
-
         def respuesta = PlatilloService.nuevoPlatillo(
             data.nombre, 
             data.menuTipo, 
@@ -88,4 +73,80 @@ class PlatilloController {
         )
         return respond( respuesta.resp, status: respuesta.status )
     }
+
+    def informacionPlatillo(){
+        if( params.uuid.size() != 32 ) {
+            return respond([success:false, mensaje: "El uuid es invalido"], status: 400)
+        }
+        def respuesta = PlatilloService.informacionPlatillo(params.uuid)
+        return respond( respuesta.resp, status: respuesta.status )
+    }
+
+    def editarPlatillo(){
+        def data = request.JSON
+        def platillo = PlatilloService.informacionPlatillo(params.uuid)
+        def fechaDisponible = platillo.resp.data.fechaDisponible
+        def platillosDisponibles = platillo.resp.data.platillosDisponibles
+
+        if(!data.nombre){
+            return respond([success:false, mensaje: "El nombre es obligatorio"], status: 400)
+        }
+        if( data.nombre.soloNumeros() ) {
+            return respond([success:false, mensaje: "El nombre debe contener letras y no solo numeros"], status: 400)
+        }
+        if( data.nombre.size() > 80 ) {
+            return respond([success:false, mensaje: "El nombre no puede ser tan largo"], status: 400)
+        }
+        if( !data.menuTipo ) {
+            return respond([success:false, mensaje: "El campo menuTipo es obligatorio"], status: 400)
+        }
+        if( data.menuTipo.size() != 32 ) {
+            return respond([success:false, mensaje: "El uuid del tipo menu es invalido"], status: 400)
+        }
+        if(!data.costo){
+            return respond([success:false, mensaje: "El costo es obligatorio"], status: 400)
+        }
+        if( !data.costo.soloNumeros() ) {
+            return respond([success:false, mensaje: "El costo debe contener solo numeros"], status: 400)
+        }
+        if( !data.descripcion ) {
+            return respond([success:false, mensaje: "La descripcion es obligatorio"], status: 400)
+        }
+        if( data.descripcion.soloNumeros() ) {
+            return respond([success:false, mensaje: "La descripcion debe contener letras y no solo numeros"], status: 400)
+        }
+        if( data.descripcion.size() > 80 ) {
+            return respond([success:false, mensaje: "La descripcion no puede ser tan largo"], status: 400)
+        }
+        if (data.fechaDisponible) {
+            try {
+                fechaDisponible = new Date(data.fechaDisponible as Long)
+            } catch (e) {
+                return respond([success:false, mensaje:"Formato de fecha invalido"], status:400)
+            }
+        }
+        if( data.platillosDisponibles){
+            if (!data.platillosDisponibles.soloNumeros()){
+                return respond([success:false, mensaje: "Los platillos disponibles deben de ser numeros"], status: 400)
+            }
+            platillosDisponibles = data.platillosDisponibles
+        }
+
+        def respuesta = PlatilloService.editarPlatillo(
+            data.nombre, 
+            data.menuTipo, 
+            fechaDisponible,
+            data.costo.toInteger(),
+            data.descripcion,
+            platillosDisponibles.toInteger(),
+            params.uuid
+        )
+        return respond(respuesta.resp, status: respuesta.status)
+    }
+
+    def editarEstatusPlatillo(){
+        def respuesta = PlatilloService.editarEstatusPlatillo( params.estatus, params.uuid )
+        return respond( respuesta.resp, status: respuesta.status )
+    }
+
 }
--- grails-app/services/com/ordenaris/restaurante/PlatilloService.groovy
+++ grails-app/services/com/ordenaris/restaurante/PlatilloService.groovy
@@ -37,7 +37,8 @@ class PlatilloService {
 
     def mapTipoMenu = { tipo, lista ->
         def obj = [
-            name: tipo.nombre,
+            uuid: tipo.uuid,
+            name: tipo.nombre
         ]
         if( lista.size() > 0 ) {
             if (lista[0].containsKey("descripcion")) {
@@ -66,6 +67,103 @@ class PlatilloService {
                 status: 200
             ]
         }catch(e){ 
+            return [
+                resp: [ success: false, mensaje: e.getMessage() ],
+                status: 500
+            ]
+        }
+    }
+
+    def informacionPlatillo( uuid ){
+        def platillo = Platillo.findByUuid(uuid)
+        if(!platillo) {
+            return [
+                resp: [ success:false, mensaje: "El platillo no existe" ],
+                status: 404
+            ]
+        }
+        if(platillo.status == 2){
+            return [
+                resp: [ success:false, mensaje: "El platillo ha sido eliminado" ],
+                status: 404
+            ]
+        }
+        def subMenu = TipoMenu.findById(platillo.tipoMenu.id)
+        // def menu = TipoMenu.findById(submenu.tipoPrincipal.id)
+
+        def respuesta = [
+            uuid:platillo.uuid,
+            nombre:platillo.nombre, 
+            descripcion:platillo.descripcion, 
+            costo:platillo.costo, 
+            status:platillo.status,
+            platillosDisponibles:platillo.platillosDisponibles,
+            fechaDisponible: platillo.fechaDisponible,
+            subMenu: mapTipoMenu(subMenu,[])
+        ]
+        return [
+            resp: [ success:true, data: respuesta ],
+            status: 200
+        ]
+    }
+
+    def editarPlatillo( nombre, menuTipo, fechaDisponible, costo, descripcion, platillosDisponibles, uuid ) {
+        try{
+            // select * from tipo_menu where uuid = UUID
+            def platillo = Platillo.findByUuid(uuid)
+
+            if( !platillo ) {
+                return [
+                    resp: [ success: false, mensaje: "El platillo no existe" ],
+                    status: 500
+                ]
+            }
+
+            def menuNuevo = TipoMenu.findByUuid(menuTipo)
+
+            if( !platillo ) {
+                return [
+                    resp: [ success: false, mensaje: "El menu no existe" ],
+                    status: 500
+                ]
+            }
+
+            platillo.nombre = nombre
+            platillo.tipoMenu = menuNuevo
+            platillo.fechaDisponible = fechaDisponible
+            platillo.costo = costo
+            platillo.descripcion = descripcion
+            platillo.platillosDisponibles = platillosDisponibles
+            platillo.save()
+
+            return [
+                resp: [ success: true ],
+                status: 200
+            ]
+        }catch(e){
+            return [
+                resp: [ success: false, mensaje: e.getMessage() ],
+                status: 500
+            ]
+        } 
+    }
+
+    def editarEstatusPlatillo( estatus, uuid ) {
+        try{
+            def platillo = Platillo.findByUuid( uuid )
+            if( !platillo ) {
+                return [
+                    resp: [success:false, mensaje: "No se encontro el platillo"],
+                    status: 500
+                ]
+            }
+            platillo.status = estatus
+            platillo.save()
+            return [
+                resp: [success:true],
+                status: 200
+            ]
+        }catch(e){
             return [
                 resp: [ success: false, mensaje: e.getMessage() ],
                 status: 500
